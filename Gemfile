275b27bc05e3b1f3724fdd2af0e073f35c92590a
Shamel Lakin <christlife4u@gmail.com>
2019-10-25 20:10:44 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in popcorn.gemspec
+gemspec
---
+++ lib/popcorn.rb
@@ -0,0 +1,6 @@
+require "popcorn/version"
+
+module Popcorn
+  class Error < StandardError; end
+  # Your code goes here...
+end
