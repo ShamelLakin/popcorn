80d96940a70801da6a97151674c26d906e02326d
Shamel Lakin <christlife4u@gmail.com>
2019-10-26 19:38:59 +0000

    user_choice

--- lib/popcorn/scraper.rb
+++ lib/popcorn/scraper.rb
@@ -2,14 +2,18 @@ class Popcorn::Scraper
  
   @@url = "https://www.imdb.com/chart/boxoffice?ref_=nv_ch_cht"
   @@doc = Nokogiri::HTML(open(@@url))
+  
+  @@titles = []
  
   def self.get_topbox_titles
-    titles = []
     @@doc.css('tbody tr td.titleColumn a').each do |title|
       titles << title.text
     end
-    titles
   end 
+  
+  def titles 
+    @@titles
+  end
   
   def self.get_topbox_revenues
     wknd_rev = []
@@ -24,6 +28,9 @@ class Popcorn::Scraper
     end 
     # binding.pry
   end 
+  
+  def self.compare_user_choice
+  end
   # Ok! #{title}'s weekend gross revenue is #{weekend rev} and it's overall gross revenue is #{gross rev}. It's been in Top Box Office Status for #{week} weeks.
   
 end 
