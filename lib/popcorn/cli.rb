36151d36f244928a58a406a62958fb497135bd63
Shamel Lakin <christlife4u@gmail.com>
2019-10-29 18:06:58 +0000

    finished

--- lib/popcorn/cli.rb
+++ lib/popcorn/cli.rb
@@ -4,15 +4,14 @@ class Popcorn::CLI
     welcome 
     show_top_ten 
     get_stats
-    goodbye
   end 
   
   def welcome
     print "Welcome to Popcorn, Where we give you the top ten box office movies and more,
     May I have your name please: "
-    name = Popcorn::User.new.name 
+    @name = Popcorn::User.new.name 
     puts
-    puts "Hi, #{name}. Here's a list of the current top 10 box office movies"
+    puts "Hi, #{@name}. Here's a list of the current top 10 box office movies"
     puts
   end 
   
@@ -28,24 +27,21 @@ class Popcorn::CLI
   
     while true
       puts "\nEnter the number of the movie you wish to find the stats on or type exit: "
-      place_holder = Popcorn::User.get_user_choice
-      choice = place_holder.class == "Integer" ? place_holder = place_holder - 1 : place_holder.to_s  
+      choice = Popcorn::User.get_user_choice
+        
       if choice == "exit"
-        exit!
-      else 
+        goodbye
+      else
+        choice = choice.to_i
         puts "\n\nOk! #{Popcorn::Scraper.get_topbox_titles[choice]}'s weekend gross revenue is #{Popcorn::Scraper.get_topbox_weekend[choice]} and it's overall gross revenue is #{Popcorn::Scraper.get_topbox_gross[choice]}. It's been in Top Box Office Status for #{Popcorn::Scraper.get_topbox_weeks[choice]} week/s.\n\nSynopsis >\n====================\n\n#{Popcorn::Scraper.get_movie_synopsis[choice]}"
       end
     end
     
-  end 
-  
-  
-  # def show_another?
-    
-  # end 
+  end
    
   def goodbye
-    puts "See you next time #{name}, Enjoy the show!"
+    puts "\nSee you next time #{@name}, Enjoy the show!"
+    exit!
   end 
 end
 
