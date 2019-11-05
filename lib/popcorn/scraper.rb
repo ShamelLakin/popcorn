f524e7f1b29e2b7284568b5e92b847e24373ed22
Shamel Lakin <christlife4u@gmail.com>
2019-11-05 16:45:39 +0000

    modifications

--- lib/popcorn/scraper.rb
+++ lib/popcorn/scraper.rb
@@ -1,13 +1,19 @@
 class Popcorn::Scraper
  
-  @@url = "https://www.imdb.com/chart/boxoffice?ref_=nv_ch_cht"
-  @@doc = Nokogiri::HTML(open(@@url))
-  
-  @@titles = []
-  @@weekend = []
-  @@gross = []
-  @@weeks = []
-  @@info =[]
+ 
+ def get_page
+   Nokogiri::HTML(open("https://www.imdb.com/chart/boxoffice?ref_=nv_ch_cht"))
+ end 
+ 
+ def scrape_movie_index(m)
+    self.get_page.css("tbody tr")
+ end 
+ 
+ def make_movies
+   scrape_movie_index.each do |m|
+      Popcorn::Movies.new_from_index_page(m)
+    end
+ end
  
   def self.get_topbox_titles
     @@doc.css('tbody tr td.titleColumn a').each do |title|
@@ -34,24 +40,47 @@ class Popcorn::Scraper
     @@gross
   end 
   
+  #-============
   
-  def self.get_topbox_weeks
-    @@doc.css("tbody tr td.weeksColumn").each do |week|
-      @@weeks << week.text.to_i
-    end
-    @@weeks
-  end 
-  
-  def self.get_movie_synopsis
-	ids = []
-    @@doc.css("tbody td.titleColumn a").each do |title|
-  		ids << title.attribute('href').value
-	end
-	ids.each do |id|
-  		@@info << Nokogiri::HTML(open("https://www.imdb.com#{id}")).css("div.summary_text").text.strip 
-    end
-    @@info
+  def initialize(title = nil, wknd = nil, gross = nil, weeks = nil)
+    @title = title
+    @wknd = wknd 
+    @gross = gross 
+    @weeks = weeks
   end
+  
+   @@all = []
+
+  def self.new_from_index_page(m)
+    self.new(
+      m.css("td.titleColumn").text,
+      m.css("td.ratingColumn").text,
+      m.css("td.ratingColumn").text,
+      m.css("td.weeksColumn").text
+      )
+  end
+
+  def self.all
+    @@all
+  end
+  
+#   def self.get_topbox_weeks
+#     @@doc.css("tbody tr td.weeksColumn").each do |week|
+#       @@weeks << week.text.to_i
+#     end
+#     @@weeks
+#   end 
+  
+#   def self.get_movie_synopsis
+# 	ids = []
+#     @@doc.css("tbody td.titleColumn a").each do |title|
+#   		ids << title.attribute('href').value
+# 	end
+# 	ids.each do |id|
+#   		@@info << Nokogiri::HTML(open("https://www.imdb.com#{id}")).css("div.summary_text").text.strip 
+#     end
+#     @@info
+#   end
     
   
 end 
